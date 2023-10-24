2950c763526009fe7b43c364204b1850d65980e9
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2023-10-24 14:34:25 +0100

    Use environment variable for api url

--- lib/redux/slices/gameStateSlice/fetchGameState.ts
+++ lib/redux/slices/gameStateSlice/fetchGameState.ts
@@ -3,7 +3,7 @@ import { GameState } from "@/app/api/types";
 export const fetchGameState = async (
   action: "start" | "hit" | "stand",
 ): Promise<GameState> => {
-  const response = await fetch("http://localhost:3000/api/", {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ action }),
