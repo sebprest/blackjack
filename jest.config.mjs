3641f6a6df319193c7640eebb0cc1b83c5b2a97e
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2023-10-25 16:19:49 +0100

    Install and configure jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,9 +8,10 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
- 
+  setupFilesAfterEnv: ['./app/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/lib(.*)$': '<rootDir>/lib$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
 }
  
